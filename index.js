c1d714446d1e299fb96cd3885a485e649f363fb0
Kevin Lu <37918520+1n48yg@users.noreply.github.com>
2023-01-11 01:54:54 -0800

    Uploaded existing progress
    Sets up a barebones structure for the project.

---
+++ index.js
@@ -0,0 +1,26 @@
+document.getElementById("testButton").onclick = function()
+{
+    var nc = document.getElementById("test");
+    nc.innerHTML = "<input type=\"text\" id=\"e34\" size=\"1\"></input>";
+
+    var o;
+    var a1, a2, a3, b1, b2, b3;
+
+    for (var row = 1; row <= 3; row++)
+    {
+        for (var col = 1; col <= 3; col++)
+        {
+            o = document.getElementById("o" + row + col);
+
+            a1 = parseInt(document.getElementById("e" + row + 1).value);
+            a2 = parseInt(document.getElementById("e" + row + 2).value);
+            a3 = parseInt(document.getElementById("e" + row + 3).value);
+
+            b1 = parseInt(document.getElementById("b" + 1 + col).value);
+            b2 = parseInt(document.getElementById("b" + 2 + col).value);
+            b3 = parseInt(document.getElementById("b" + 3 + col).value);
+
+            o.innerHTML = (a1 * b1 + a2 * b2 + a3 * b3);
+        }
+    }
+}
