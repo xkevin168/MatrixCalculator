f09affaf3bc2518ee9362b5532d2654b752fb255
Kevin Lu <37918520+1n48yg@users.noreply.github.com>
2023-02-01 22:21:50 -0800

    Initial blueprint on adding rows

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Action of multiplying AB
 document.getElementById("calcButton").onclick = function()
 {
     //var nc = document.getElementById("test");
@@ -23,4 +24,10 @@ document.getElementById("calcButton").onclick = function()
             o.innerHTML = (a1 * b1 + a2 * b2 + a3 * b3);
         }
     }
+}
+
+document.getElementById("incRowA").onclick = function()
+{
+    var mat1 = document.getElementById("matrix1");
+    mat1.innerHTML += "\<p id=\"matrix1row3\"\> \<input type=\"text\" id=\"e31\" size=\"1\"\> \<input type=\"text\" id=\"e32\" size=\"1\"\> \<input type=\"text\" id=\"e33\" size=\"1\"\> \</p\>";
 }
